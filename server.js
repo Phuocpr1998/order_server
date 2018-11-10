df805c7844c4ba70cfd6a451bf03f8aae64979f5
Châu Hoàng Phúc <chauhoangphuc432@gmail.com>
2018-11-10 20:11:33 +0700

    add some event not complete

--- server.js
+++ server.js
@@ -7,18 +7,59 @@ server.listen(process.env.PORT || 3000);
 
 console.log("Co nguoi connect ne");
 
-//io.sockets.on('connection', function (socket) {
-	
-//  console.log("Co nguoi connect ne");
+var users = [];
+
+io.sockets.on('connection', function (socket) {
   
-// io.sockets.emit('serverguitinnhan', { noidung: "okbaby" });
+	//io.sockets.emit('serverguitinnhan', { noidung: "okbaby" });
   
-//  socket.on('servernhantinnhan', function (data) {
-	// emit toi tat ca moi nguoi
-//	io.sockets.emit('serverguitinnhan', { noidung: data });
-	
-	// emit tới máy nguoi vừa gửi
-//	socket.emit('serverguitinnhan', { noidung: data });
-//  });
+  socket.on('register', function (email) {//email of user ---------- send from user when connect
+	if(email){
+			socket.email = email;
+			users.push(socket);
+	}
+  });
   
-//});
+  //structure of order
+  
+  //{
+//		email_owner
+//		email_guest
+//		id_rest
+//		order: [ 
+//					{id_dish, number}
+//				]
+  //}
+  
+  socket.on('send_order', function(data){ //
+	  var order = JSON.parse(data);
+	  var index = users.findIndex(soc => soc.email === order.email_owner);
+	  if(index != -1){
+		  var id = users[index].id;
+		  io.to(id).emit('receive_order', data);
+	  }else{
+		  socket.emit('result', {status:404, message:'Order fail!'});
+	  }
+  });
+  
+  
+   //structure of result
+  
+  //{
+//		email_guest
+//		status
+//		message
+  //}
+  
+  socket.on('send_result', function(response){ //
+	  var resp = JSON.parse(response);
+	  var index = users.findIndex(soc => soc.email === resp.email_guest);
+	  if(index != -1){
+		  var id = users[index].id;
+		  io.to(id).emit('receive_result', response);
+	  }else{
+		  socket.emit('result', {status:404, message:'Turn back fail!'});
+	  }
+  });
+  
+});
